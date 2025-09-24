7e8e71c310492c0b69ab787160b00e4e675b6b87
Assefa Endale <abassiendale96@gmail.com>
2025-09-24 05:58:00 -0700

    renaming app

--- ecommerce_backend/messaging_app/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-class product (models.Model):
-    name = models.CharField(max_length=200)
-    marchant = models.CharField(max_length=100)
-    booking_date = models.DateField()
